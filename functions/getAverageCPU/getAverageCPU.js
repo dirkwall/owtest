0c6cdc856e7a5992830ad4044b1eb4ec92d9d81e
dirkwall <dirk.wallerstorfer@dynatrace.com>
2020-03-16 13:56:50 +0100

    changing http.get result handling

--- functions/getAverageCPU/getAverageCPU.js
+++ functions/getAverageCPU/getAverageCPU.js
@@ -1,43 +1,35 @@
 const http = require("http");
 
 function main(params) {
+	console.log("start");
 	const url = params.url;
+	console.log("url: ", url);
 
   http.get(url, (res) => {
-		const { statusCode } = res;
+		let data = '';
 
-		if (statusCode !== 200) {
-			console.error('Request Failed. ' + `Status Code: ${statusCode}`);
-			// consume response data to free up memory
-			res.resume();
-			return;
-		}
-
-		res.setEncoding('utf8');
-		let rawData = '';
-		res.on('data', (chunk) => { rawData += chunk; });
+		res.on('data', (chunk) => {
+			data += chunk; 
+		});
 		res.on('end', () => {
-			try {
-				const parsedData = JSON.parse(rawData);
-				console.log(parsedData);
+			const parsedData = JSON.parse(data);
+			console.log(parsedData);
 
-				// example data point:
-				// { ...
-				//	 "logicalCpuCores": 16,
-				// ...}
-				const length = parsedData.length;
-				var sum = 0;
+			// example data point:
+			// { ...
+			//	 "logicalCpuCores": 16,
+			// ...}
+			const length = parsedData.length;
+			var sum = 0;
 
-				parsedData.forEach(element => {
-					sum += element.logicalCpuCores;
-				});
+			parsedData.forEach(element => {
+				sum += element.logicalCpuCores;
+			});
 
-				const average = sum/length;
+			const average = sum/length;
+			console.log("average: ", average);
 
-				return {averageCPU: average};
-			} catch (e) {
-				console.error(e.message);
-			}
+			return {averageCPU: average};
 		});
 	}).on('error', (e) => {
 		console.error(`Got error: ${e.message}`);
