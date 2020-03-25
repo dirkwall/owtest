0e4d74f0f9de7a7878f6e349006c50a992e99556
dirkwall <dirk.wallerstorfer@dynatrace.com>
2020-03-25 22:28:42 +0100

    got rid of rps parameter - not needed here

--- setup.sh
+++ setup.sh
@@ -2,15 +2,14 @@
 
 OP=$1
 NAMESPACES=$2
-MAX_USERS=$3
-MAX_RPS=$4
+USERS=$3
 CRED_FILE=./credentials.json
 
-[ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] || [ -z "$4" ] && echo "Four parameters OPERATION, NAMESPACES, MAX_USERS, MAX_RPS needed!" && exit 1
+[ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] && echo "Three parameters OPERATION, NAMESPACES, USERS needed!" && exit 1
 
 function delete_users() {
   for n in $(seq 1 $NAMESPACES); do
-    for u in $(seq 1 $MAX_USERS); do
+    for u in $(seq 1 $USERS); do
       wskadmin user delete user$u -ns ns$n
       echo "user$u ns$n deleted"
     done
@@ -20,11 +19,11 @@ function delete_users() {
 function create_users() {
   echo "[" > $CRED_FILE
   for n in $(seq 1 $NAMESPACES); do
-    for u in $(seq 1 $MAX_USERS); do
+    for u in $(seq 1 $USERS); do
       CREDENTIALS=$(wskadmin user create user$u -ns ns$n)
       echo "user$u ns$n $CREDENTIALS"
       COMMA=","
-      if [ $u -eq $MAX_USERS ] && [ $n -eq $NAMESPACES ]; then
+      if [ $u -eq $USERS ] && [ $n -eq $NAMESPACES ]; then
         COMMA=""
       fi
       echo "  {\"namespace\":\"ns$n\", \"credentials\":\"$CREDENTIALS\"}$COMMA" >> $CRED_FILE
@@ -95,5 +94,3 @@ elif [ "$OP" = "d" ]; then
 else
   echo "OPERATION must be either 'c' for create or 'd' for delete" && exit 1
 fi
-
-# generate config.json
