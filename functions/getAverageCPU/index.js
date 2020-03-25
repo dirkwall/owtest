08aec63d4371c754317f8e9ea39e117a59d095fe
dirkwall <dirk.wallerstorfer@dynatrace.com>
2020-03-25 12:18:37 +0100

    implementation with async/await

--- functions/getAverageCPU/index.js
+++ functions/getAverageCPU/index.js
@@ -1,30 +1,22 @@
 const needle = require("needle");
 
-function func(params) {
+async function func(params) {
 	console.log("start");
 	const url = params.url;
 	console.log("url: ", url);
 
-  return new Promise(function(resolve, reject) {
-    needle('get', url)
-      .then(function(res) {
-        const data = res.body;
-        const length = data.length;
-        var sum = 0;
+	const result = await needle('get', url);
+	const data = result.body;
+	const length = data.length;
+	let sum = 0;
 
-        data.forEach(element => {
-          sum += element.logicalCpuCores;
-        });
+	data.forEach(element => {
+		sum += element.logicalCpuCores;
+	});
 
-        const average = sum/length;
-        console.log("average: ", average);
-        resolve({averageCPU: average});
-      })
-      .catch(function(err) {
-        console.error(`Got error: ${err}`);
-        reject({error: err});
-      });
-  });
+	const average = sum/length;
+	console.log("average: ", average);
+	return { averageCPU: average };
 }
 
 exports.main = func;
