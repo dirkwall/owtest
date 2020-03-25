a734c1cd65e451c365a27f727073e5fefc845a6c
dirkwall <dirk.wallerstorfer@dynatrace.com>
2020-03-25 12:48:31 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ OP=$1
 NAMESPACES=$2
 MAX_USERS=$3
 MAX_RPS=$4
+CRED_FILE=./credentials.json
 
 [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] || [ -z "$4" ] && echo "Four parameters OPERATION, NAMESPACES, MAX_USERS, MAX_RPS needed!" && exit 1
 
@@ -17,21 +18,20 @@ function delete_users() {
 }
 
 function create_users() {
-  echo "[\n" > credentials.json
+  echo "[" > $CRED_FILE
   for n in $(seq 1 $NAMESPACES); do
     for u in $(seq 1 $MAX_USERS); do
       CREDENTIALS=$(wskadmin user create user$u -ns ns$n)
       echo "user$u ns$n $CREDENTIALS"
       B64CREDS=$(echo -n $CREDENTIALS | base64 -w0)
-      echo "  {\"namespace\":\"ns$n\", \"credentials\":\"$B64CREDS\"}"
-      if [ $u eq $MAX_USERS]; then
-        echo ",\n"
-      else
-        echo "\n"
+      COMMA=","
+      if [ $u -eq $MAX_USERS ] && [ $n -eq $NAMESPACES ]; then
+        COMMA=""
       fi
+      echo "  {\"namespace\":\"ns$n\", \"credentials\":\"$B64CREDS\"}$COMMA" >> $CRED_FILE
     done
   done
-  echo "]\n" >> credentials.json
+  echo "]" >> $CRED_FILE
 }
 
 if [ "$OP" = "c" ]; then
@@ -45,4 +45,3 @@ fi
 # create functions
 
 # generate config.json
-
