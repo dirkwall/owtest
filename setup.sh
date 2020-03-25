15eb465a34442a8d5c4e0201790dff62dff95ed7
dirkwall <dirk.wallerstorfer@dynatrace.com>
2020-03-25 22:07:51 +0100

    implemented creation of functions

--- setup.sh
+++ setup.sh
@@ -34,14 +34,51 @@ function create_users() {
   echo "]" >> $CRED_FILE
 }
 
+function prepare_function() {
+  CURDIR=`pwd`
+  cd $1
+  if [ ! -f function.zip ]; then
+    npm install &> /dev/null
+    zip -r function.zip *
+  fi
+  cd $CURDIR
+}
+
+function create_functions() {
+  #iterate over functions
+  jq -c '.[]' functions.json | while read i; do
+    F_NAME=`echo $i | jq -r '.name'`
+    F_PATH=`echo $i | jq -r '.path'`
+    F_FILE=`echo $i | jq -r '.file'`
+    F_ZIP=`echo $i | jq -r '.zip'`
+    F_KIND=`echo $i | jq -r '.kind'`
+    F_CONCURRENCY=`echo $i | jq -r '.concurrency'`
+    F_MEMORY=`echo $i | jq -r '.memory'`
+    F_TIMEOUT=`echo $i | jq -r '.timeout'`
+
+    if [ $F_ZIP == "true" ]; then
+      prepare_function $F_PATH
+    fi
+
+    # iterate over namespaces
+    for n in $(seq 1 $NAMESPACES); do
+      NS="ns$n"
+      jq -c '[.[] | select( .namespace == "'$NS'")][0]' credentials.json | while read i; do
+        CREDENTIAL=`echo $i | jq -r '.credentials'`
+        wsk action create $NS$F_NAME $F_FILE -c $F_CONCURRENCY --kind $F_KIND -m $F_MEMORY -t $F_TIMEOUT -u $CREDENTIAL
+      done
+    done
+  done
+}
+
 if [ "$OP" = "c" ]; then
   create_users
 elif [ "$OP" = "d" ]; then
   delete_users
+elif [ "$OP" = "cf" ]; then
+  create_functions
 else
   echo "OPERATION must be either 'c' for create or 'd' for delete" && exit 1
 fi
-
-# create functions
 
 # generate config.json
