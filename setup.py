e8e78bc2c073377bc7443845ee2bfa7692112bbb
dirkwall <dirk.wallerstorfer@dynatrace.com>
2020-03-26 11:15:23 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,9 +11,8 @@ parser.add_argument('namespaces', help="Number of namespaces", type=int, choices
 parser.add_argument('users', help="Number of users", type=int, choices=range(1,21))
 args = parser.parse_args()
 
-credentials = {}
-
 if args.operation == "create":
+  credentials = {}
   for n in range(args.namespaces):
     ns_name = "ns{}".format(n)
     credentials[ns_name] = {}
@@ -22,10 +21,10 @@ if args.operation == "create":
     for u in range(args.users):
       user_name = "user{}".format(u)
       cmd = "wskadmin user create {} -ns {}".format(user_name, ns_name)
-      #ret = subprocess.run(cmd.split(" "), stdout=subprocess.PIPE, universal_newlines=True)
-      #credentials[ns_name][user_name] = str(ret.stdout).strip('\n')
-      #print("{} {} created".format(user_name, ns_name))
-    #print(credentials)
+      ret = subprocess.run(cmd.split(" "), stdout=subprocess.PIPE, universal_newlines=True)
+      credentials[ns_name][user_name] = str(ret.stdout).strip('\n')
+      print("{} {} created".format(user_name, ns_name))
+    
     with open('credentials.json', 'w') as outfile:
       json.dump(credentials, outfile)
 
@@ -34,8 +33,6 @@ if args.operation == "create":
       data = json.load(json_file)
     
     for f in data:
-      print("##################################")
-      print(f)
       # prepare function
       if f['zip'] and not os.path.exists(f['file']):
         subprocess.run(["npm", "install", "--quiet"], cwd=f['path'])
