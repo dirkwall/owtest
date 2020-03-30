97fb278aa229b4800d5e70e82c97eae5fad994b0
dirkwall <dirk.wallerstorfer@dynatrace.com>
2020-03-30 09:10:45 +0200

    adding weight as an attribute to the function

--- setup.py
+++ setup.py
@@ -55,18 +55,18 @@ if args.operation == "create":
   for n in range(args.namespaces):
     ns_name = "ns{}".format(n)
     users = math.ceil(args.users/(n+1))
+    weight = math.ceil(args.rps/(n+1))
     for u in range(users):
       user_name = "user{}".format(u)
       for f in functions:
-        weight = math.ceil(args.rps/(n+1))
-        for i in range(weight):
-          r = {}
-          r['url'] = "https://{}/api/v1/namespaces/_/actions/{}{}?blocking=true".format(args.host, ns_name, f['name'])
-          r['credentials'] = credentials[ns_name][user_name]
-          r['auth_header'] = base64.b64encode(credentials[ns_name][user_name].encode()).decode('utf-8')
-          r['params'] = f['params']
-          requests.append(r)
-  
+        r = {}
+        r['weight'] = weight
+        r['url'] = "https://{}/api/v1/namespaces/_/actions/{}{}?blocking=true".format(args.host, ns_name, f['name'])
+        r['credentials'] = credentials[ns_name][user_name]
+        r['auth_header'] = base64.b64encode(credentials[ns_name][user_name].encode()).decode('utf-8')
+        r['params'] = f['params']
+        requests.append(r)
+
   # create functions
   with open('requests.json', 'w') as outfile:
     json.dump(requests, outfile)
